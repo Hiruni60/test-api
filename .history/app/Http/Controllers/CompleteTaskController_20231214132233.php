b5c4cda0f3a1705c8a1762e1f7b3a51bed814479
Hiruni <piumikahiruni22@gmail.com>
2023-12-14 13:36:17 +0530

    rest api for crud

---
+++ .history/app/Http/Controllers/CompleteTaskController_20231214132233.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class CompleteTaskController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request, Task $task)
+    {
+       $task->is_completed =$request->is_completed;
+       $task->save();
+
+       return TaskResource::m
+    }
+}
--- .history/database/seeders/DatabaseSeeder_20231130143539.php
+++ .history/database/seeders/DatabaseSeeder_20231130143539.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
-use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Task::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
